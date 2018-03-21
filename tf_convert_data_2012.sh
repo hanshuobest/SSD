8a48f86b2ed348e910fde0dc9f5676f0491e5510
“shuo.han” <shuo.han@wormpex.com>
2018-03-21 11:33:26 +0800

    3-21

--- tf_convert_data_2012.sh
+++ tf_convert_data_2012.sh
@@ -1,6 +1,7 @@
 ＃filename = tf_convert_data.sh
 #!/bin/bash
 #This is a shell script to convert Pascal VOC datasets(2007 and 2012) into TF-Records only.
+# 这是一个将VOC datasets（2007和2012）转成TF-Records的脚本
 
 #Directory where the original dataset is stored
 DATASET_DIR=/home/han/DataSets/VOCdevkit-2012/VOC2012/
